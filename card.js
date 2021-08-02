483e79f503bdb1680c10ddbbcd7452b37dfaf246
oezge sebisteri <sebisteri@outlook.com>
2021-08-02 11:42:57 +0200

    corrects border-div

--- card.js
+++ card.js
@@ -1,12 +1,7 @@
 function Card(props) {
-    function classes() {
-        const bg = props.bgcolor ? ' bg-' + props.bgcolor : ' ';
-        const txt = props.txtcolor ? ' text-' + props.txtcolor : ' text-white';
-        return 'card mb-3 ' + bg + txt;
-    }
-
+    
     return (
-        <div className='myDiv' style={{maxWidth: "23rem"}}>
+        <div className='myDiv'>
             <div className="card-body">
                 {props.title && (<h5 className="card-title">{props.title}</h5>)}
                 {props.text && (<h5 className="card-text">{props.text}</h5>)}
